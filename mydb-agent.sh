7e7e88fb8650c1d5069604e161a58d71deaaeced
zhangjin <23231269@qq.com>
2024-09-19 12:29:18 +0800

    增加变量定义提示。

--- mydb-agent.sh
+++ mydb-agent.sh
@@ -5,15 +5,20 @@ mydb_log(){
   echo `date --rfc-3339=seconds` [mydb-agent-main] $1
 }
 
-# main loop
-while true
-do
-  mydb_log "start run agent cmd!"
-  AGENT_CMD=`curl -s ${MYDB_CENTER_HOST}/agent/mysql/getAgentCmd`
-  if [ $? -eq 0 ]; then
-    bash -c "${AGENT_CMD}"
-  else
-    mydb_log "can't get agent cmd, please check MYDB_CENTER_HOST config!"
-  fi
-  sleep 60
-done
+if [ -z "${MYDB_CENTER_HOST}" ]; then
+    mydb_log "MYDB_CENTER_HOST is empty, please check MYDB_CENTER_HOST config!"
+else
+    mydb_log "MYDB_CENTER_HOST=${!MYDB_CENTER_HOST}"
+    # main loop
+    while true
+    do
+      mydb_log "start run agent cmd!"
+      AGENT_CMD=`curl -s ${MYDB_CENTER_HOST}/agent/mysql/getAgentCmd`
+      if [ $? -eq 0 ]; then
+        bash -c "${AGENT_CMD}"
+      else
+        mydb_log "can't get agent cmd, please check MYDB_CENTER_HOST config!"
+      fi
+      sleep 60
+    done
+fi
